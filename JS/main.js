db517396c4541f90a9c9a485ab1443e3929b25e5
Julio Sabino <julio.c.moreira11@live.com>
2025-06-07 19:21:44 -0300

    Inclusão dos primeiros recursos lógicos

--- JS/main.js
+++ JS/main.js
@@ -37,4 +37,65 @@
         }
     });
 
-})(jQuery); // <- fechamento correto da função auto-executável
+    $('.testimonial-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: true,
+        nav: false,
+        animateOut: 'slideOutDown',
+        animateIn: 'slideInDown',
+        autoplay: false,
+        smartSpeed: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    //Blog Carousel
+    $("blog-carousel").owlCarousel({
+        loop: true,
+        itens: 3,
+        dots: false,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+
+    //jQuery meanMenu
+    $(".main-menu nav").meanmenu({
+        meanScriinWidth: "991",
+        meanmenuContainer: ".mobile"
+    });
+
+    //Mail Chip
+    $('#mc-form').ajaxChip({
+        language: "en",
+        callback: mailChimpResponse,
+        url:("http://themeshaven.us8.list-manage.com/subscribe/post?u=759ce8a8f4f1037e021ba2922&id=a2452237f8")
+    })
+
+})
+
